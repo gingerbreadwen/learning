06266284c54d5f2bb8b0d143835e87c633d56730
gingerbreadwen <gingerbreadwen@163.com>
2020-05-27 08:23:59 +0800

    add project

---
+++ proj0/Body.java
@@ -0,0 +1,30 @@
+public class Body{
+	double xxPos; 
+	double yyPos;
+	double xxVel;
+	double yyVel;
+	double mass;
+	String imgFileName;
+	public Body(double xP, double yP, double xV,
+              double yV, double m, String img){
+		this.xxPos = xP;
+		this.yyPos = yP;
+		this.xxVel = xV;
+		this.yyVel = yV;
+		this.mass = m;
+		this.imgFileName = img;
+	}
+	public Body(Body b){
+		this.xxPos = b.xxPos;
+		this.yyPos = b.yyPos;
+		this.xxVel = b.xxVel;
+		this.yyVel = b.yyVel;
+		this.mass = b.mass;
+		this.imgFileName = b.imgFileName;
+
+	}
+
+	public double calcDistance(Body rocinante){
+		return Math.s
+	}
+}
